0bbb3b9dcc717ab0c4c2beffaba4f007802b5d5b
yanxj <yanxj@dist.cn>
2019-03-04 17:59:23 +0800

    first commit

---
+++ worker.js
@@ -0,0 +1,12 @@
+
+self.onmessage = function (e) {
+    var data = e.data;
+    console.log('worker:',  data.length);
+};
+const dic = [6, 9, 12];
+const start = 0;
+function handle(data) {
+    data.map((item)=>{
+        
+    })
+}
